01401c27f7394bbe23597fe42847ed5418042903
Sankin Ilya <sankin1195@mail.ru>
2023-06-17 21:34:06 +0300

    Изменен

---
+++ HW10/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW10
+{
+    public class Menu
+    {
+        public int ChooseEmployee()
+        {            
+            Console.WriteLine("1-Cons \n2-Man");
+            int result = int.Parse(Console.ReadLine());
+            return result;
+        }
+
+        public int ChooseMoves()
+        {
+            Console.WriteLine("1- показать всех клиентов \n2- найти клиента по id");
+            int result = int.Parse(Console.ReadLine());
+            return result;
+        }
+
+        public int ChooseUserId()
+        {
+            Console.WriteLine("Введите id пользователя для просмотра информации");
+            int id = int.Parse(Console.ReadLine());
+            return id;
+        }
+
+        public int ChooseCorrect()
+        {
+            Console.WriteLine("Изменить номер телефона?\n1 - Да;\n2 - Нет.");
+
+
+        }
+    }
+}
--- HW10/Program.cs
+++ HW10/Program.cs
@@ -6,17 +6,20 @@
         {
             Repository rep = new Repository("Consultant.txt");
             rep.Load();
-            Consultant con = new Consultant();
-            Console.WriteLine("1 - Зайти под конультантом;\n2 - зайти под менеджером.");
-            int choose = int.Parse(Console.ReadLine());
+            Consultant con;
+            Menu menu = new Menu();
+
+            int choose = menu.ChooseEmployee();
+
             if (choose == 1)
             {
-                Console.WriteLine("Введите id пользователя для просмотра информации");
-                int id = int.Parse(Console.ReadLine());
+                con = new Consultant();
+                int id = menu.ChooseUserId();
                 User user = rep.FindId(id);
                 Console.WriteLine(con.Read(user).ToString());
                 Console.WriteLine("Изменить номер телефона?\n1 - Да;\n2 - Нет.");
                 int change = int.Parse(Console.ReadLine());
+
                 if(change == 1)
                 {
                     Console.Write("Введите новый номер:");
@@ -25,11 +28,13 @@
                     editedUser.Phone = newPhone;
                     con.Edit(user,editedUser);
                 }
+
                // Тест Тест
+
             }
-            else if(choose == 2) 
+            else 
             {
-                Consultant man = new Manager();
+                con = new Manager();
 
 
             }
