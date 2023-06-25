db604dea260e0a04df93dd4ae7a8c69eb737113b
Sankin Ilya <sankin1195@mail.ru>
2023-06-25 16:56:58 +0300

    +

--- HW10/Program.cs
+++ HW10/Program.cs
@@ -14,57 +14,52 @@
 
             if (choose == 1)
             {
-                con = new Consultant();
-                int id = menu.ChooseUserId();
-                User user = rep.FindId(id);
-                Console.WriteLine(con.Read(user).ToString());
-
-                int change = menu.ChooseCorrect();
-
-                if (change == 5)
-                {
-                    var editedUser = new User(user);
-                    editedUser.Phone = correct.ChangePhone();
-                    con.Edit(user, editedUser);
-                }
-                else Console.WriteLine("Нет прав");
+              con = new Consultant();
             }
-            else
+            else con = new Manager();
+
+            int id = menu.ChooseUserId();
+            User user = rep.FindId(id);
+            Console.WriteLine(con.Read(user).ToString());
+
+            int change = menu.ChooseCorrect();
+
+            if (change == 5)
             {
-                con = new Manager();
-                int id = menu.ChooseUserId(); //дублирование 
-                User user = rep.FindId(id); //дублирование 
-                Console.WriteLine(con.Read(user).ToString()); //дублирование 
-
-                int change = menu.ChooseCorrect(); //дублирование 
-
-                User editUser = new User(user);
-
-                switch (change)
-                {
-                    case 1:
-                        editUser.FirstName = correct.ChangeFirstName();
-                        break;
-
-                    case 2:
-                        editUser.LastName = correct.ChangeLastName();
-                        break;
-
-                    case 3:
-                        editUser.Patronymic = correct.ChangePatronymic();
-                        break;
-
-                    case 4:
-                        editUser.Passport = correct.ChangePassport();
-                        break;
-
-                    case 5:
-                        editUser.Phone = correct.ChangePhone();
-                        break;
-                }
-
-
+                var editedUser = new User(user);
+                editedUser.Phone = correct.ChangePhone();
+                con.Edit(user, editedUser);
             }
+            else Console.WriteLine("Нет прав");
+
+
+            User editUser = new User(user);
+
+            switch (change)
+            {
+                case 1:
+                    editUser.FirstName = correct.ChangeFirstName();
+                    break;
+
+                case 2:
+                    editUser.LastName = correct.ChangeLastName();
+                    break;
+
+                case 3:
+                    editUser.Patronymic = correct.ChangePatronymic();
+                    break;
+
+                case 4:
+                    editUser.Passport = correct.ChangePassport();
+                    break;
+
+                case 5:
+                    editUser.Phone = correct.ChangePhone();
+                    break;
+            }
+
+
+
 
 
 
